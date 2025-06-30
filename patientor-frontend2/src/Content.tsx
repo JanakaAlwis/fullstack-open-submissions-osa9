853aa9b18a51f7296c98ed452abd71213387d2a8
JanakaAlwis <janaka.donmanuwelge@student.hamk.fi>
2025-06-30 22:39:20 +0300

    Exercise 9.15 done

---
+++ patientor-frontend2/src/Content.tsx
@@ -0,0 +1,18 @@
+type CoursePart = {
+  name: string
+  exerciseCount: number
+}
+
+const Content = ({ parts }: { parts: CoursePart[] }) => {
+  return (
+    <div>
+      {parts.map((part) => (
+        <p key={part.name}>
+          {part.name} {part.exerciseCount}
+        </p>
+      ))}
+    </div>
+  )
+}
+
+export default Content
