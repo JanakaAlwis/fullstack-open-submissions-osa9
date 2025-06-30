d5ddde98991d02caf3984e7641d3c10c945faa5e
JanakaAlwis <janaka.donmanuwelge@student.hamk.fi>
2025-06-30 23:37:33 +0300

    Exercise 9.16 done

--- patientor-frontend2/src/App.tsx
+++ patientor-frontend2/src/App.tsx
@@ -1,38 +1,18 @@
-import Header from './Header'
-import Content from './Content'
-import Total from './Total'
-
-type CoursePart = {
-  name: string
-  exerciseCount: number
-}
+import Header from "./components/Header";
+import Content from "./components/Content";
+import Total from "./components/Total";
+import { courseParts } from "./data";
 
 const App = () => {
-  const courseName = "Half Stack application development"
-  const courseParts: CoursePart[] = [
-    {
-      name: "Fundamentals",
-      exerciseCount: 10
-    },
-    {
-      name: "Using props to pass data",
-      exerciseCount: 7
-    },
-    {
-      name: "Deeper type usage",
-      exerciseCount: 14
-    }
-  ]
-
-  const totalExercises = courseParts.reduce((sum, part) => sum + part.exerciseCount, 0)
+  const courseName = "Half Stack application development";
 
   return (
     <div>
       <Header name={courseName} />
-      <Content parts={courseParts} />
-      <Total total={totalExercises} />
+      <Content courseParts={courseParts} />
+      <Total courseParts={courseParts} />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
---
+++ patientor-frontend2/src/components/Part.tsx
@@ -0,0 +1,50 @@
+import type { CoursePart } from "../types";
+
+const assertNever = (value: never): never => {
+  throw new Error(
+    `Unhandled discriminated union member: ${JSON.stringify(value)}`
+  );
+};
+
+const Part = ({ part }: { part: CoursePart }) => {
+  switch (part.kind) {
+    case "basic":
+      return (
+        <div>
+          <h3>{part.name}</h3>
+          <p>Exercises: {part.exerciseCount}</p>
+          <p>Description: {part.description}</p>
+        </div>
+      );
+    case "group":
+      return (
+        <div>
+          <h3>{part.name}</h3>
+          <p>Exercises: {part.exerciseCount}</p>
+          <p>Group projects: {part.groupProjectCount}</p>
+        </div>
+      );
+    case "background":
+      return (
+        <div>
+          <h3>{part.name}</h3>
+          <p>Exercises: {part.exerciseCount}</p>
+          <p>Description: {part.description}</p>
+          <p>Background material: <a href={part.backgroundMaterial}>{part.backgroundMaterial}</a></p>
+        </div>
+      );
+    case "special":
+      return (
+        <div>
+          <h3>{part.name}</h3>
+          <p>Exercises: {part.exerciseCount}</p>
+          <p>Description: {part.description}</p>
+          <p>Requirements: {part.requirements.join(", ")}</p>
+        </div>
+      );
+    default:
+      return assertNever(part);
+  }
+};
+
+export default Part;
