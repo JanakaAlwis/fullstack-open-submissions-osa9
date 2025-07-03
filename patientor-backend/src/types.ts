cc901527853d42f3ced72dea16b4064cf61d7ae8
JanakaAlwis <janaka.donmanuwelge@student.hamk.fi>
2025-07-04 00:42:43 +0300

    Exercises 9.23 done

--- patientor-backend/src/types.ts
+++ patientor-backend/src/types.ts
@@ -1,3 +1,5 @@
+// src/types.ts
+
 export enum Gender {
   Male = 'male',
   Female = 'female',
@@ -7,19 +9,77 @@ export enum Gender {
 export interface Diagnosis {
   code: string;
   name: string;
-  latin?: string; 
+  latin?: string;
 }
 
-export interface Entry {}
+/**
+ * Base type for all entries
+ */
+interface BaseEntry {
+  id: string;
+  date: string;
+  description: string;
+  specialist: string;
+  diagnosisCodes?: Array<Diagnosis['code']>;
+}
 
+/**
+ * HealthCheck entry
+ */
+export enum HealthCheckRating {
+  "Healthy" = 0,
+  "LowRisk" = 1,
+  "HighRisk" = 2,
+  "CriticalRisk" = 3
+}
+
+export interface HealthCheckEntry extends BaseEntry {
+  type: "HealthCheck";
+  healthCheckRating: HealthCheckRating;
+}
+
+/**
+ * Hospital entry
+ */
+export interface HospitalEntry extends BaseEntry {
+  type: "Hospital";
+  discharge: {
+    date: string;
+    criteria: string;
+  };
+}
+
+/**
+ * OccupationalHealthcare entry
+ */
+export interface OccupationalHealthcareEntry extends BaseEntry {
+  type: "OccupationalHealthcare";
+  employerName: string;
+  sickLeave?: {
+    startDate: string;
+    endDate: string;
+  };
+}
+
+/**
+ * Union type for all entries
+ */
+export type Entry =
+  | HospitalEntry
+  | OccupationalHealthcareEntry
+  | HealthCheckEntry;
+
+/**
+ * Patient types
+ */
 export interface Patient {
   id: string;
   name: string;
   dateOfBirth: string;
   ssn: string;
-  gender: Gender;     
+  gender: Gender;
   occupation: string;
-  entries: Entry[];   
+  entries: Entry[];
 }
 
 export type NewPatient = Omit<Patient, 'id'>;
